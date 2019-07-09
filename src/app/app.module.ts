47f11e98cf6a2ab6e19ff3fe8c99d655e8bbde9d
unknown <manzoor.alam@thinktac.com>
2019-07-09 14:14:27 +0530

    imported storage and services in the app.module.ts file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,15 +9,29 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { IonicStorageModule } from '@ionic/storage';
+
+import { AuthenticationService } from './services/Authentication.service';
+import { AuthGuardService } from './services/auth-guard.service';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule, 
+    IonicModule.forRoot(),
+    IonicStorageModule.forRoot(),
+    AppRoutingModule
+  ],
+
   providers: [
     StatusBar,
     SplashScreen,
+    AuthGuardService,
+    AuthenticationService,
+    
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }

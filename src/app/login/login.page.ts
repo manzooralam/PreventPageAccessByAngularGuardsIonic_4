d17d5e276094e501ff45a400e4a1ec82d5f057fd
unknown <manzoor.alam@thinktac.com>
2019-07-09 15:39:07 +0530

    Update Login and Dashboard Components

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from '../services/authentication.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  constructor( 
+    private authService: AuthenticationService
+    ) { }
 
   ngOnInit() {
   }
-
+  loginUser(){
+    this.authService.login()
+  }
 }

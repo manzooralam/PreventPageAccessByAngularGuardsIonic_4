5740aa4a0244c64759399413b00020bde9b5b19d
unknown <manzoor.alam@thinktac.com>
2019-07-09 15:05:24 +0530

    At Login time takes set userInfo to storage also authState.next(true). At Logout time remove userinfo and also authState(false). Here authState = new BehaviorSubject(false); When open the app we are checking userInfo from storage , is true then set the authState.next(true);

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -1,9 +1,55 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { Storage } from '@ionic/storage';
+import { ToastController, Platform } from '@ionic/angular';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
 
-  constructor() { }
+  authState = new BehaviorSubject(false);
+ 
+  constructor(
+    private router: Router,
+    private storage: Storage,
+    private platform: Platform,
+    public toastController: ToastController
+  ) {
+    this.platform.ready().then(() => {
+      this.ifLoggedIn();
+    });
+  }
+ 
+  ifLoggedIn() {
+    this.storage.get('USER_INFO').then((response) => {
+      if (response) {
+        this.authState.next(true);
+      }
+    });
+  }
+ 
+ 
+  login() {
+    var dummy_response = {
+      user_id: 'manzoor.alam@thinktac.com',
+      user_name: 'manzoor'
+    };
+    this.storage.set('USER_INFO', dummy_response).then((response) => {
+      this.router.navigate(['dashboard']);
+      this.authState.next(true);
+    });
+  }
+ 
+  logout() {
+    this.storage.remove('USER_INFO').then(() => {
+      this.router.navigate(['login']);
+      this.authState.next(false);
+    });
+  }
+ 
+  isAuthenticated() {
+    return this.authState.value;
+  }
 }

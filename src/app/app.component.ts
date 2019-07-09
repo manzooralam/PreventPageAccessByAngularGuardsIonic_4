6514343e9cf491b874359038a64dd41f53c2a550
unknown <manzoor.alam@thinktac.com>
2019-07-09 15:31:28 +0530

    Check User Authentication on App Load

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,9 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { AuthenticationService } from './services/Authentication.service';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
@@ -12,7 +15,11 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private router: Router,
+
+    private authenticationService: AuthenticationService
+
   ) {
     this.initializeApp();
   }
@@ -21,6 +28,16 @@ export class AppComponent {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+
+      this.authenticationService.authState.subscribe(state => {
+        if (state) {
+          this.router.navigate(['dashboard']);
+        } else {
+          this.router.navigate(['login']);
+        }
+      });
+
     });
+
   }
 }

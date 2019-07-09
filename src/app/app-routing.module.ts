cc811de2e5a7954e1e8db3ccd14d475dea930a47
unknown <manzoor.alam@thinktac.com>
2019-07-09 15:16:26 +0530

    Added canActivate()  method in AuthGuardService to check Authentication or No

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AuthGuardService } from './services/auth-guard.service';
+
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  // { path: '', redirectTo: 'home', pathMatch: 'full' },
+  // { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  // { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
+  // { path: 'dashboard', loadChildren: './dashboard/dashboard.module#DashboardPageModule' },
+
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
-  { path: 'dashboard', loadChildren: './dashboard/dashboard.module#DashboardPageModule' },
+  {
+    path: 'dashboard',
+    loadChildren: './dashboard/dashboard.module#DashboardPageModule',
+    canActivate: [AuthGuardService]
+  }
 ];
 
 @NgModule({
--- src/app/services/auth-guard.service.ts
+++ src/app/services/auth-guard.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { AuthenticationService } from './authentication.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuardService {
 
-  constructor() { }
+  constructor( public authenticationService: AuthenticationService) { }
+
+  canActivate(): boolean {
+    return this.authenticationService.isAuthenticated();
+  }
 }
